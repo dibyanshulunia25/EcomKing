dffec2de391b78b276d8cb3282f7b398c597cc6f
dibyanshulunia25 <dibyanshulunia098@gmail.com>
2025-07-28 18:22:18 +0530

    Products display

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -2,16 +2,18 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { Link } from 'react-router-dom';
 
-const ProductItem = ({id,image,name,price}) => {
-const {currency} = useContext(ShopContext);
+const ProductItem = ({ id, image, name, price }) => {
+  const { currency } = useContext(ShopContext);
 
   return (
     <>
-        <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
-            <div className="overflow-hidden">
-                <img src={image[0]} alt={name} className="w-full h-64 object-cover transition-transform duration-300 hover:scale-105" />
-            </div>
-        </Link>
+      <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
+        <div className="overflow-hidden">
+          <img src={image[0]} alt={name} className="w-full h-64 object-cover transition-transform duration-300 hover:scale-105" />
+        </div>
+        <h3 className='text-lg font-medium'>{name}</h3>
+        <p className='text-gray-400'>{price}</p>
+      </Link>
     </>
   )
 }
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -132,15 +132,26 @@
 // export default Collection
 
 
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets';
 import Title from '../components/Title';
+import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
 
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
+  const [filterProducts, setFilterProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
+
+  useEffect(() => {
+    setFilterProducts(products);
+  }
+    , [])
+
+
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
 
@@ -152,17 +163,17 @@ const Collection = () => {
         {/* Category filter */}
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
           <p className='mb-3 test-sm font-medium'>Categories</p>
-            <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
-              <p className='flex gap-2 cursor-pointer'>
-                <input type="checkbox" className='w-3' value={'Men'} />Men
-              </p>
-              <p className='flex gap-2 cursor-pointer'>
-                <input type="checkbox" className='w-3' value={'Women'} />Women
-              </p>
-              <p className='flex gap-2 cursor-pointer'>
-                <input type="checkbox" className='w-3' value={'Kids'} />Kids
-              </p>
-            </div>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+            <p className='flex gap-2 cursor-pointer'>
+              <input type="checkbox" className='w-3' value={'Men'} />Men
+            </p>
+            <p className='flex gap-2 cursor-pointer'>
+              <input type="checkbox" className='w-3' value={'Women'} />Women
+            </p>
+            <p className='flex gap-2 cursor-pointer'>
+              <input type="checkbox" className='w-3' value={'Kids'} />Kids
+            </p>
+          </div>
         </div>
         {/* Sub category filter */}
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
@@ -184,7 +195,20 @@ const Collection = () => {
       {/*Right side Products list */}
       <div className='flex-1'>
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
-          <Title text1={'ALL'} text2={"collections"}></Title>
+          <Title text1={'ALL'} text2={"COLLECTIONS"} />
+          {/* Sort options */}
+          <select className='border-2 border-gray-300 text-sm rounded-md mr-10 p-2'>
+            <option value="relevant">Newest Arrivals</option>
+            <option value="low-high">Price: Low to High</option>
+            <option value="high-low">Price: High to Low</option>
+          </select>
+        </div>
+
+        {/* map products */}
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+          {filterProducts.map((item,index) => (
+            <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image} />
+          ))}
         </div>
       </div>
     </div>
