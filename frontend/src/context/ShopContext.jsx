f22a59d58a1f329159367ff1e3b67d11446abcab
dibyanshulunia25 <dibyanshulunia098@gmail.com>
2025-07-29 17:12:21 +0530

    minor changes

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
@@ -6,9 +6,11 @@ export const ShopContext = createContext();
 const ShopContextProvider = ({ children }) => {
     const currency = '₹'; // Define the currency symbol you want to use
     const delivery_fee = 50; // Define the delivery charges
-    
+    const [search,setSearch] = useState('');
+    const [showSearch,setShowSearch] = useState(true);
+
     const contextValue = {
-        products, currency, delivery_fee,
+        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch
     };
 
     return (
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -132,7 +132,7 @@
 // export default Collection
 
 
-import { use, useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets';
 import Title from '../components/Title';
@@ -209,7 +209,7 @@ const Collection = () => {
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
 
       {/* Filter options */}
-      <div className='min-w-60 mx-10'>
+      <div className='min-w-50 mx-10'>
         <p onClick={() => setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS
           <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} src={assets.dropdown_icon} alt="" />
         </p>
@@ -250,7 +250,7 @@ const Collection = () => {
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
           <Title text1={'ALL'} text2={"COLLECTIONS"} />
           {/* Sort options */}
-          <select onChange={(e) => setSortType(e.target.value)} className='border-2 border-gray-300 text-sm rounded-md mr-10 p-2'>
+          <select onChange={(e) => setSortType(e.target.value)} className='border-2 border-gray-300 text-sm rounded-md mr-10 p-2 sm:text-xs'>
             <option value="relevant">Newest Arrivals</option>
             <option value="low-high">Price: Low to High</option>
             <option value="high-low">Price: High to Low</option>
