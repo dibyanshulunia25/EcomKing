7f48bb4a397a8c37e7071ff8dad755686452d051
dibyanshulunia25 <dibyanshulunia098@gmail.com>
2025-07-30 18:06:27 +0530

    ----------

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { ShopContext } from "../context/ShopContext";
 
 const Product = () => {
+
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
+
+  const fetchProductData = async () => {
+
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item);
+        console.log(item);
+        return null
+      }
+    });
+
+  }
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
+
   return (
     <div>Product</div>
   )
